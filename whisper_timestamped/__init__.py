a836c50d816a118ac3a4104b6b1c318bbb92a91e
gehhehhejiedjhfdhuhfejcendckjn <preducsharma0077889@gmail.com>
2025-02-15 18:56:09 +0530

    Update __init__.py

--- whisper_timestamped/__init__.py
+++ whisper_timestamped/__init__.py
@@ -1,4 +1,4 @@
-from whisper import available_models, _download, _MODELS # defined in __init__.py
+
 from whisper import audio, decoding, model, normalizers, tokenizer, utils
 from whisper.audio import load_audio, log_mel_spectrogram, pad_or_trim
 from whisper.decoding import DecodingOptions, DecodingResult, decode, detect_language
